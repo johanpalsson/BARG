575fe6a8be029366d43f868441ff2f80c3de3021
Johan Pålssonjoh <johan.palsson85@gmail.com>
2022-03-30 15:31:09 +0200

    Add project files.

---
+++ BARG/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BARG.Startup))]
+namespace BARG
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
